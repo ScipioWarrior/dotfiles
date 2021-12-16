d453c6c438568eba1f5b0665217c48fcab74e31c
RJ Duane <git@rjd.xyz>
2021-12-16 12:06:35 -0500

    Switch to default Manjaro i3 directory

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -7,7 +7,7 @@
 ln -sv ~/dotfiles/Spacemacs ~/.spacemacs
 
 # i3 Windows Manager
-ln -sv ~/dotfiles/i3 ~/.config/i3/config
+ln -sv ~/dotfiles/i3 ~/.i3/config
 
 # Vivaldi browser
 ln -sv ~/dotfiles/Vivaldi.css /opt/vivaldi/resources/vivaldi/style/custom.css
