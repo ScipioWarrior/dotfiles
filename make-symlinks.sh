6a252aa83340f11d0fc716f5e8e107927b83a472
RJ Duane <git@rjd.xyz>
2021-12-19 15:13:50 -0500

    Add default Manjaro i3status config

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -9,5 +9,8 @@ ln -sv ~/dotfiles/Spacemacs ~/.spacemacs
 # i3 Windows Manager
 ln -sv ~/dotfiles/i3 ~/.i3/config
 
+# i3bar/status 
+ln -sv ~/dotfiles/i3status ~/.i3status/config
+
 # Vivaldi browser
 ln -sv ~/dotfiles/Vivaldi.css /opt/vivaldi/resources/vivaldi/style/custom.css
